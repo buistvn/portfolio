7e87a2287cd70fda8ad492e9591f03430d5e38c7
buistvn <buist@oregonstate.edu>
2021-03-11 17:56:18 -0800

    Changed about section, updated resume

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,7 @@ function About() {
                 <p className="aboutParagraph">Hi, I'm Steven. 
                     I'm currently a third year student at Oregon State University studying Computer Science. 
                     Since high school, I became interested in programming with a focus in software engineering.
-                    I'm excited that I have the opportunity to explore the many incredible things that can be done with coding.
-                    In the meantime, I've been working on side projects to learn new tools and technologies which you can check out below!
+                    I've been working on side projects to learn new tools and technologies which you can check out below!
                 </p>
             </div>
         </div>
